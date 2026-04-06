c70c716a8ace8c5dfaca79e276bbde226ce1b498
Aarditya07 <aaryanaditya217@gmail.com>
2026-04-06 08:36:52 +0530

    Enhance HelloApp to greet multiple names
    Added functionality to greet multiple names passed as arguments.

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,23 @@
 public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Default case
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            // Enhanced for loop
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            // Remove trailing ", " using substring
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
